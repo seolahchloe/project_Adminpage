67a45c2661e9d33513cba42b52fcfeabae875fcb
Seolah Chung <seolahchloe.chung@gmail.com>
2021-01-31 21:07:16 +0900

    Create Amdin Project

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,9 @@
+package com.example.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // localhost:8080/api
+public class GetController {
+}
