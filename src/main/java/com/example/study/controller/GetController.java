a8b82ccde4f4fcb187931f30bea5ca7bd63b87ed
Seolah Chung <seolahchloe.chung@gmail.com>
2021-01-31 21:54:25 +0900

    [feat] getParameter로 id와 pw 호출
    id와 password를 호출해주기 위해 @GetMapping 어노테이션과 @RequestParam을 사용하여 해당 값을 지정

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,8 +1,6 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api") // localhost:8080/api
@@ -11,5 +9,14 @@ public class GetController {
     @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
     public String getRequest() {
         return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter")  // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+        System.out.println("id: " + id);
+        System.out.println("password: " + password);
+
+        return id + password;
+
     }
 }
