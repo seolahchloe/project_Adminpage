a7b4595f660d24f7ae4c104cc5a76991ee247d44
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-03 00:47:43 +0900

    [test] DELETE를 위한 Test 메소드
    Update와 마찬가지로 findById에 파라미터로 삭제할 대상을 넣어주고 ifPresent(만약 대상이 존재한다면)를 통해 선택된 유저 삭제
    
    DELETE이기 때문에 리턴하지 않으며 deleteUser를 Optional로 지정

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -52,8 +52,20 @@ public class UserRepositoryTest extends StudyApplicationTests {
         });
     }
 
-    @DeleteMapping("/api/user")
-    public void delete(@RequestParam Long id) {
-        
+    @Test
+    public void delete() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+
+        if(deleteUser.isPresent()){
+            System.out.println("데이터 존재: " + deleteUser.get());
+        }else {
+            System.out.println("데이터 삭제 데이터 없음");
+        }
     }
 }
