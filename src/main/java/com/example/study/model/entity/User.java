d365906def075084579113bc8fb14238713b7020
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 21:38:54 +0900

    [feat] DB Column 이름 명시

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.study.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ public class User {
 
     private Long id;
 
+    @Column(name = "account")
     private String account;
 
     private String email;
