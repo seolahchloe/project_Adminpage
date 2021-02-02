ab09200923b951af70f5d29186a291265dc60b9f
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 22:35:22 +0900

    [test] Test를 위한 Object 설정

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -3,20 +3,27 @@ package com.example.study.repository;
 import com.example.study.StudyApplication;
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Test
     public void create() {
         User user = new User();
-        user.setAccount();
-        user.setEmail();
-        user.setPhoneNumber();
-        user.setCreatedAt();
-        user.setCreatedBy();
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser01@gmail.com");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser: " + newUser);
     }
 
     public void read() {
