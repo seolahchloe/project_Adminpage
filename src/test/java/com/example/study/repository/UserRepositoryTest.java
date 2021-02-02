9fef3d784fd627652674743c5d5146b0f66ad910
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 23:05:53 +0900

    [test] Update를 위한 Test 메소드
    Update를 위해서는 특정 데이터를 Select 해야하며 Update할 user도 반드시 존재해야한다.
    또한 Update이므로 userRepository에 해당 유저를 반드시 저장한다.

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -29,19 +28,25 @@ public class UserRepositoryTest extends StudyApplicationTests {
     }
 
     @Test
-    public User read(@RequestParam Long id) {
+    public void read() {
         Optional<User> user = userRepository.findById(2L);
 
         user.ifPresent(selectUser -> {
             System.out.println("user: " + selectUser);
             System.out.println("email: " + selectUser.getEmail());
         });
-
-        return user;
     }
-
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
     public void delete() {
