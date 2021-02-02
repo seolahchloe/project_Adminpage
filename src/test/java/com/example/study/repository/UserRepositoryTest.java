911247dd0efd159345a9be22c10e68df1c8f505b
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 22:17:12 +0900

    [test] UserRepositoryTest 생성 및 StudyApplicationTest 상속
    Repository의 CRUD 테스트를 위한 메소드 선언

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+import com.example.study.StudyApplicationTests;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+    
+    public void create() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+}
