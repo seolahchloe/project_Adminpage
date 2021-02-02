3ce321da5dc0fcff56ccf0c29d2363d51fc61756
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 21:43:36 +0900

    [feat] 식별자와 전략 설정
    식별자로 Id 설정과 DBMS로 MySQL을 사용하기 때문에 Identity 전략 설정

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -1,15 +1,15 @@
 package com.example.study.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity // == table
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String account;
 
     private String email;
