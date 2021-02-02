f6eeea79de439c77e93a888f4d46b92bfe4b255c
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 22:19:20 +0900

    [test] Autowired 추가
    Dependancy Injection(DI)을 위한 기능

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,9 +2,14 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplication;
 import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+
+    private UserRepository userRepository;
     
     public void create() {
 
