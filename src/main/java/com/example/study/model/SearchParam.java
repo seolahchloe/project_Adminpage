b94ad26339ef04bb9ae82b8a0d23d196526ae6ab
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-02 20:25:10 +0900

    [feat] AllArgsConstructor 추가
    모든 매개변수(arguements)를 가지는 생성사 추가를 위한 @AllArgsConstructor 추가

--- src/main/java/com/example/study/model/SearchParam.java
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -1,9 +1,10 @@
 package com.example.study.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-
+@AllArgsConstructor // 모든 매개변수(arguement)를 가지는 생성자 추가
 public class SearchParam {
 
     private String account;
