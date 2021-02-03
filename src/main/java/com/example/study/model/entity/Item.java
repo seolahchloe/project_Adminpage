390fc8fe4ee47a943acf64686e4db78e8b4d2fe0
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-03 23:37:31 +0900

    [feat] 모든 생성자와 기본 생성자 추가

--- src/main/java/com/example/study/model/entity/Item.java
+++ src/main/java/com/example/study/model/entity/Item.java
@@ -1,6 +1,8 @@
 package com.example.study.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Item {
 
