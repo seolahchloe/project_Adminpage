29edcc3472a88052e67ebf4ceef273997d177d31
Seolah Chung <seolahchloe.chung@gmail.com>
2021-02-03 23:40:22 +0900

    [feat] Database 테이블에 맞는 Entity 생성 (OrderDetail)
    Lombok,
    기본 생성자와 모든 생성자,
    Entity 선언
    
    DB의 order_detail 컬럼과 연동

---
+++ src/main/java/com/example/study/model/entity/OrderDetail.java
@@ -0,0 +1,15 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderDetail {
+
+}
